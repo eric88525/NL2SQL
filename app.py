b3cce4c5cf060cd43361b23af363dd5b914c6c02
eric88525 <eric885251@gmail.com>
2020-10-19 00:59:50 +0800

    add index & kendo

--- app.py
+++ app.py
@@ -11,24 +11,32 @@ app.config["JSON_AS_ASCII"] = False
 
 
 dbService = DBService(dbConfig)   
-modelService = ModelService(model_config,dbConfig)
+#modelService = ModelService(model_config,dbConfig)
 
 @app.route("/")
 def index():
     
-    return "123"
+    return render_template("index.html")
 
 
 @app.route("/api/tablelist",methods=["GET"])
 def get_table_list():
-    result = dbService.get_table_list()
+    table_list = dbService.get_table_list()
+    result = [ {"Text": row[:15] ,"Value":row  }for row in table_list ]
     return jsonify(result)
 
 @app.route("/api/table",methods=["POST"])
 def get_talbes():
     request_data = request.get_json()
-    result = dbService.get_table(request_data["table_name"])
-    return jsonify(result)
+    columns = dbService.get_headers(request_data["table_name"])[0]
+    rows  = dbService.get_table(request_data["table_name"])
+    data = []
+    for r in rows:
+        temp = {}
+        for i,col in enumerate(columns):
+            temp[col] = r[i]
+        data.append(temp)
+    return jsonify( { "columns":columns,"data":data  }  )
 
 @app.route("/api/headers",methods=["POST"])
 def get_headers():
@@ -36,11 +44,12 @@ def get_headers():
     result = dbService.get_headers(request_data["table_name"])
     return jsonify(result)
 
+"""
 @app.route("/api/sql",methods=["POST"])
 def get_sql():
     request_data = request.get_json()
     result = modelService.get_sql(request_data["question"],request_data["table_name"])
     return jsonify(result)
-
+"""
 if __name__ == '__main__':
     app.run()
