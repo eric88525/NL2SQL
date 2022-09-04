524ca15205dc85a36a1fe5fb4c53d1831fe53f82
eric88525 <39957434+eric88525@users.noreply.github.com>
2022-09-04 17:14:38 +0800

    Replace ajax as fetch

--- static/js/script.js
+++ static/js/script.js
@@ -1,78 +1,53 @@
 
-$(document).ready(function () {
-  // create float window
-  $("#floatWindow").kendoWindow({
-    width: '1000px',
-    height: 'auto',
-    title: "SQL result",
-    visible: false,
-    modal: true,
-    actions: [
-      "Close"
-    ],
-  }).data("kendoWindow").center();
-
+window.onload = function () {
   createDropDownList('/api/tablelist');
+}
+// create float window
+function createDropDownList(url) {
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (content) {
 
+      console.log(content);
 
+      let tableList = document.getElementById('tableList');
 
-  function createDropDownList(url) {
-    content = []
-    $.ajax({
-      type: "get",
-      url: url,
-      dataType: "json",
-      async: false,
-      success: function (data) {
-        content = data;
-      }
+      content.forEach(element => {
+        let li = document.createElement('li');
+        let a = document.createElement('a');
+        a.classList.add('dropdown-item');
+        a.innerHTML = element.Value;
+        // send table name to showTable function
+        a.addEventListener('click', showTable.bind(null, element.Value));
+        li.appendChild(a);
+        tableList.appendChild(li);
+      });
     });
-
-    let tableList = document.getElementById('tableList');
-
-    tableList.removeChild(tableList.childNodes[0]);
-    ul = document.createElement('ul');
-    ul.classList.add('dropdown-menu');
-    ul.classList.add('dropdown-menu-dark');
-    tableList.appendChild(ul);
-
-    content.forEach(element => {
-      var li = document.createElement('li');
-      var a = document.createElement('a');
-      a.classList.add('dropdown-item');
-      a.innerHTML = element.Value;
-      // send table name to showTable function
-      a.addEventListener('click', showTable.bind(null, element.Value));
-      li.appendChild(a);
-      ul.appendChild(li);
-    });
-  }
-
-
-
-
-  // execute SQL command and show table
-
-});
-
+}
 
 // post query to backend and show result on resultText
 function getSql() {
-  $.ajax({
-    type: "post",
-    url: '/api/sql',
-    contentType: 'application/json;charset=UTF-8',
-    data: JSON.stringify({
-      question:  document.getElementById('questionText').value ,
-      table_name: document.getElementById('tableName').innerHTML 
-    }),
-    async: false,
-    success: function (data) {
-      var resultText = document.getElementById('resultText');
-      resultText.innerHTML = data;
-    }
-  });
 
+  let url = '/api/sql';
+  let data = {
+    question: document.getElementById('questionText').value,
+    table_name: document.getElementById('tableName').innerHTML
+  };
+
+  fetch(url, {
+    method: 'POST', // or 'PUT'
+    body: JSON.stringify(data), // data can be `string` or {object}!
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    })
+  }).then(res => res.json())
+    .catch(error => console.error('Error:', error))
+    .then(function (response) {
+      let resultText = document.getElementById('resultText');
+      resultText.innerHTML = response;
+    });
 }
 
 
@@ -83,28 +58,21 @@ function showTable(tableId) {
   let tableName = document.getElementById('tableName');
   tableName.innerHTML = tableId;
 
-  var columns = []
-  var datas = []
-
-  $.ajax({
-    type: "post",
-    url: '/api/table',
-    contentType: 'application/json;charset=UTF-8',
-    data: JSON.stringify({
-      table_name: tableId
-    }),
-    async: false,
-    success: function (data) {
-      columns = data.columns,
-        datas = data.datas
-    }
+  let data = {table_name:tableId};
+  let url = '/api/table';
+  fetch(url, {
+    method: 'POST', // or 'PUT'
+    body: JSON.stringify(data), // data can be `string` or {object}!
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    })
+  }).then(res => res.json())
+  .catch(error => console.error('Error:', error))
+  .then(function (response) {
+    let table = createTable(response.columns, response.datas);
+    let demoTable = document.getElementById('demoTable');
+    demoTable.removeChild(demoTable.childNodes[0]); demoTable.appendChild(table);
   });
-  var table = createTable(columns, datas);
-
-  let demoTable = document.getElementById('demoTable');
-  demoTable.removeChild(demoTable.childNodes[0]);
-  $('#demoTable').append(table);
-
 }
 
 
@@ -127,50 +95,46 @@ function createTable(columns, datas) {
   // create table header
   tr = document.createElement('tr');
   columns.forEach(element => {
-    var row = document.createElement('th');
+    let row = document.createElement('th');
     row.innerHTML = element.Name
-
     tr.appendChild(row);
-    headers.push(element.Index)
+    headers.push(element.Index);
   });
   thead.appendChild(tr)
 
   // create table body
   datas.forEach(element => {
-    var row = document.createElement('tr');
+    let row = document.createElement('tr');
     headers.forEach(key => {
-      var rowData = document.createElement('td');
+      let rowData = document.createElement('td');
       rowData.classList.add("bar");
-      rowData.innerHTML = element[key]
+      rowData.innerHTML = element[key];
       row.appendChild(rowData);
     });
     tbody.appendChild(row);
   });
-  return table
+  return table;
 }
 
-
+// execute SQL command and show table
 function runSql() {
+
   console.log('run sql...')
-  var columns = [];
-  var datas = [];
-  $.ajax({
-    type: "post",
-    url: '/api/runsql',
-    contentType: 'application/json;charset=UTF-8',
-    data: JSON.stringify({
-      sql: $("#resultText").val()
-    }),
-    async: false,
-    success: function (data) {
-      columns = data.columns,
-        datas = data.datas
-    }
+
+  fetch( '/api/runsql',{
+    method: 'POST', // or 'PUT'
+    body: JSON.stringify({sql:document.getElementById('resultText').value}), // data can be `string` or {object}!
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    })
+  }).then(response=>response.json())
+  .then(
+    function (response) {
+      console.log(response);
+      // popout modal
+      table = createTable(response.columns, response.datas);
+      let modalTable = document.getElementById('modalTable');
+      modalTable.removeChild(modalTable.childNodes[0]);
+      modalTable.appendChild(table);
   });
-  table = createTable(columns, datas);
-
-  $("#floatDemoTable table").remove();
-  $('#floatDemoTable').append(table);
-
-  $("#floatWindow").data("kendoWindow").open();
-}
+};
